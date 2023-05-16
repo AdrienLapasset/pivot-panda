6d8e4bf8484e1a490276fecb421b069619bd6a7e
Clément Lapasset <clement.lapasset@gmail.com>
2023-05-16 12:33:20 +0200

    init coming-soon

--- src/pages/coming-soon.js
+++ src/pages/coming-soon.js
@@ -11,7 +11,9 @@ const StyledContainer = styled.div`
 const comingSoon = () => {
   return (
     <StyledContainer>
-      <h1>coming soon</h1>
+      <h1>le site de pivot panda se refait une beauté !</h1>
+      <p>Nous travaillons actuellement sur notre nouveau site. Pendant ce temps, nous vous prions de bien vouloir patienter.</p>
+      <p>N'hésitez pas à nous contacter à l'adresse suivante : contact@pivotpanda.com.</p>
     </StyledContainer>
   );
 };
