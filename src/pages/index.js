90e7425d69b43bfb837b250ec165bd9f9baed2bb
Clément Lapasset <clement.lapasset@gmail.com>
2023-05-17 16:08:29 +0200

    migrate coming soon component in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,75 @@
 import * as React from "react";
+import styled from "styled-components";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "components/Layout";
+import PPlogo from "assets/logo.svg";
 
-import ComingSoon from "./coming-soon";
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 15px;
+  text-align: center;
+  @media ${(props) => props.theme.minWidth.lg} {
+    padding: 25px;
+  }
+  .logo {
+    height: 170px;
+  }
+  h1 {
+    color: ${(props) => props.theme.colors.black};
+    text-transform: uppercase;
+    font-size: 28px;
+    font-weight: 700;
+    margin: 40px 0;
+    @media ${(props) => props.theme.minWidth.lg} {
+      font-size: 33px;
+    }
+  }
+  p {
+    color: ${(props) => props.theme.colors.black};
+    margin-bottom: 10px;
+    @media ${(props) => props.theme.minWidth.lg} {
+      margin-bottom: 25px;
+    }
+  }
+  a {
+    display: inline;
+    text-decoration: underline;
+  }
+  .gatsby-image-wrapper {
+    margin-top: -30px;
+    z-index: -1;
+    @media ${(props) => props.theme.minWidth.sm} {
+      margin-top: -80px;
+    }
+  }
+`;
 
 const IndexPage = () => {
   return (
     <Layout>
-      <ComingSoon />
+      <StyledContainer>
+        <img className="logo" src={PPlogo} alt="Logo" />
+        <h1>Le site de pivot panda se refait une beauté !</h1>
+        <p>
+          Nous travaillons actuellement sur notre nouveau site. Pendant ce
+          temps, nous vous prions de bien vouloir patienter.{" "}
+        </p>
+        <p>
+          Vous pouvez nous contacter à l'adresse suivante :{" "}
+          <a href="mailto:contact@pivotpanda.com">contact@pivotpanda.com</a>
+        </p>
+        <StaticImage
+          src="../assets/coming-soon-panda.png"
+          alt="Panda"
+          height={500}
+        />
+      </StyledContainer>
     </Layout>
   );
 };
 
 export default IndexPage;
-
