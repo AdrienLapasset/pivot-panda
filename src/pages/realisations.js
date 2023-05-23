04e3d30f8cc67934f8163cd2aaf71912f3d66c8a
Clément Lapasset <clement.lapasset@gmail.com>
2023-05-23 19:10:41 +0200

    add functional filters for projects list

--- src/pages/realisations.js
+++ src/pages/realisations.js
@@ -1,11 +1,11 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Layout from "components/Layout";
 import PageHero from "../components/pages/realisations/PageHero";
 import CategoryFilters from "../components/pages/realisations/CategoryFilters";
 import ProjectsList from "../components/pages/realisations/ProjectsList";
 
 const Realisations = () => {
-  const [selectedCategory, setSelectedCategory] = React.useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const handleCategoryChange = (selectedCategory) => {
     setSelectedCategory(selectedCategory);
