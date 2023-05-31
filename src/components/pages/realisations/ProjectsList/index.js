926420fd81d4fc143f7bd192bede966a4a6d714a
Clément Lapasset <clement.lapasset@gmail.com>
2023-05-31 18:01:27 +0200

    Add Text component

--- src/components/pages/realisations/ProjectsList/index.js
+++ src/components/pages/realisations/ProjectsList/index.js
@@ -3,6 +3,8 @@ import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 import Grid from "components/global/Grid";
+import PageContainer from "components/global/PageContainer";
+import Text from "components/global/Text";
 
 const StyledProjectsList = styled.div`
   @media ${(props) => props.theme.minWidth.md} {
@@ -27,11 +29,7 @@ const StyledProjectContainer = styled.div`
   }
 `;
 
-const StyledTextLabel = styled.div`
-  font-size: 12px;
-  opacity: 0.5;
-  line-height: 15px;
-  text-transform: uppercase;
+const StyledText = styled(Text)`
   @media ${(props) => props.theme.minWidth.md} {
     display: none;
   }
@@ -56,23 +54,16 @@ const StyledProjectInfo = styled.div`
   }
 `;
 
-const StyledProjectCategory = styled.div`
+const StyledMobileText = styled.div`
   @media ${(props) => props.theme.minWidth.md} {
-    font-size: 12px;
-    opacity: 0.5;
-    line-height: 15px;
-    text-transform: uppercase;
+    display: none;
   }
 `;
 
-const StyledDesktopTags = styled.div`
+const StyledDesktopText = styled(Text)`
   display: none;
   @media ${(props) => props.theme.minWidth.md} {
     display: block;
-    font-size: 12px;
-    opacity: 0.5;
-    line-height: 15px;
-    text-transform: uppercase;
   }
 `;
 
@@ -114,46 +105,55 @@ const ProjectsList = ({ selectedCategory }) => {
     : projects;
 
   return (
-    <StyledProjectsList>
-      {filteredProjects.map((project, i) => {
-        const image = getImage(project.image.asset);
-        let gridColumn;
-        let paddingLeft;
-        if (i % 6 === 0 || i % 6 === 5) {
-          gridColumn = "span 3";
-        } else {
-          gridColumn = "span 2";
-        }
+    <PageContainer>
+      <StyledProjectsList>
+        {filteredProjects.map((project, i) => {
+          const image = getImage(project.image.asset);
+          let gridColumn;
+          let paddingLeft;
+          if (i % 6 === 0 || i % 6 === 5) {
+            gridColumn = "span 3";
+          } else {
+            gridColumn = "span 2";
+          }
 
-        if (i % 3 !== 0) {
-          paddingLeft = "30px";
-        }
+          if (i % 3 !== 0) {
+            paddingLeft = "30px";
+          }
 
-        return (
-          <StyledProjectContainer
-            key={i}
-            gridColumn={gridColumn}
-            paddingLeft={paddingLeft}
-          >
-            <Grid>
-              <StyledProjectName>
-                <StyledTextLabel>Projet</StyledTextLabel>
-                {project.name}
-              </StyledProjectName>
-              <StyledProjectInfo>
-                <StyledTextLabel>Catégorie</StyledTextLabel>
-                <StyledProjectCategory>
-                  {project.category}
-                </StyledProjectCategory>
-                <StyledDesktopTags>{project.city}</StyledDesktopTags>
-                <StyledDesktopTags>{project.surface} m²</StyledDesktopTags>
-              </StyledProjectInfo>
-            </Grid>
-            <StyledGatsbyImage image={image} alt={project.name} />
-          </StyledProjectContainer>
-        );
-      })}
-    </StyledProjectsList>
+          return (
+            <StyledProjectContainer
+              key={i}
+              gridColumn={gridColumn}
+              paddingLeft={paddingLeft}
+            >
+              <Grid>
+                <StyledProjectName>
+                  <StyledText type="label">Projet</StyledText>
+                  {project.name}
+                </StyledProjectName>
+                <StyledProjectInfo>
+                  <StyledText type="label">Catégorie</StyledText>
+                  <StyledMobileText type="label">
+                    {project.category}
+                  </StyledMobileText>
+                  <StyledDesktopText type="label">
+                    {project.category}
+                  </StyledDesktopText>
+                  <StyledDesktopText type="label">
+                    {project.city}
+                  </StyledDesktopText>
+                  <StyledDesktopText type="label">
+                    {project.surface} m²
+                  </StyledDesktopText>
+                </StyledProjectInfo>
+              </Grid>
+              <StyledGatsbyImage image={image} alt={project.name} />
+            </StyledProjectContainer>
+          );
+        })}
+      </StyledProjectsList>
+    </PageContainer>
   );
 };
 
