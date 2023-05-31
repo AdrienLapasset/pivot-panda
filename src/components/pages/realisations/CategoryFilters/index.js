49e9809b2092d3ac1b7a350dfa6c0c5c2c53b3b5
Clément Lapasset <clement.lapasset@gmail.com>
2023-06-01 00:40:16 +0200

    Fix negative margins on projects list

--- src/components/pages/realisations/CategoryFilters/index.js
+++ src/components/pages/realisations/CategoryFilters/index.js
@@ -2,23 +2,22 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { useStaticQuery, graphql } from "gatsby";
 import Grid from "components/global/Grid";
+import PageContainer from "components/global/PageContainer";
 import RadioButton from "./RadioButton";
 
 const StyledContainer = styled.div`
   height: 45px;
   border-top: 1px solid ${(props) => props.theme.colors.black};
   border-bottom: 1px solid ${(props) => props.theme.colors.black};
-  margin: 0 -15px;
   @media ${(props) => props.theme.minWidth.md} {
     height: 75px;
-    margin: 0 -25px;
   }
 `;
 
 const MobileDropdown = styled.select`
   grid-column: 1 / 3;
   font-size: 12px;
-  padding: 15px;
+  padding: 15px 0;
   line-height: 15px;
   text-transform: uppercase;
   border: none;
@@ -32,7 +31,7 @@ const StyledRadioGroup = styled(Grid)`
   display: none;
   @media ${(props) => props.theme.minWidth.md} {
     display: grid;
-    margin: 0 25px;
+    padding: 25px 0;
     align-items: center;
     height: 100%;
   }
@@ -52,38 +51,35 @@ const CategoryFilters = ({ onCategoryChange }) => {
 
   const [selectedCategory, setSelectedCategory] = useState("Tout les projets");
 
-  const handleChange = (category) => {
-    const selectedCategory = category.target.value;
-    setSelectedCategory(selectedCategory);
-    onCategoryChange(selectedCategory);
+  const handleChange = (e) => {
+    const newCategory = e.target.value;
+    setSelectedCategory(newCategory);
+    onCategoryChange(newCategory);
   };
 
   return (
     <StyledContainer>
-      <Grid>
-        <MobileDropdown>
+      <PageContainer>
+        <Grid>
+          <MobileDropdown value={selectedCategory} onChange={handleChange}>
+            {allCategories.map((i, category) => (
+              <option key={i} value={category}>
+                {category}
+              </option>
+            ))}
+          </MobileDropdown>
+        </Grid>
+        <StyledRadioGroup>
           {allCategories.map((category) => (
-            <option
+            <RadioButton
               key={category}
               category={category}
               onChange={handleChange}
               selectedCategory={selectedCategory}
-            >
-              {category}
-            </option>
+            />
           ))}
-        </MobileDropdown>
-      </Grid>
-      <StyledRadioGroup>
-        {allCategories.map((category) => (
-          <RadioButton
-            key={category}
-            category={category}
-            onChange={handleChange}
-            selectedCategory={selectedCategory}
-          />
-        ))}
-      </StyledRadioGroup>
+        </StyledRadioGroup>
+      </PageContainer>
     </StyledContainer>
   );
 };
--- src/components/pages/realisations/ProjectsList/index.js
+++ src/components/pages/realisations/ProjectsList/index.js
@@ -11,21 +11,17 @@ const StyledProjectsList = styled.div`
     display: grid;
     grid-template-columns: repeat(7, 1fr);
     grid-column-gap: ${(props) => props.theme.columnGap.desktop};
-    margin: 0 -10px;
   }
 `;
 
 const StyledProjectContainer = styled.div`
-  padding: 15px;
-  margin: 0 -15px;
-  border-bottom: 1px solid ${(props) => props.theme.colors.black};
-  border-right: 1px solid ${(props) => props.theme.colors.black};
+  padding-top: 15px;
   @media ${(props) => props.theme.minWidth.md} {
-    padding: 25px;
-    padding-left: ${(props) => props.paddingLeft};
-    grid-column: ${(props) => props.gridColumn};
     display: flex;
     flex-direction: column-reverse;
+    position: relative;
+    grid-column: ${(props) => props.gridColumnProject};
+    padding: 25px 0 0 ${(props) => props.paddingLeft};
   }
 `;
 
@@ -48,9 +44,10 @@ const StyledProjectName = styled.div`
 const StyledProjectInfo = styled.div`
   grid-column: 3 / 5;
   @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 1 / 8;
+    grid-column: ${(props) => props.gridColumnProjectTags};
     display: flex;
     justify-content: space-between;
+    padding-bottom: 25px;
   }
 `;
 
@@ -78,6 +75,39 @@ const StyledGatsbyImage = styled(GatsbyImage)`
   }
 `;
 
+const MobileBottomLine = styled.div`
+  height: 1px;
+  width: 100vw;
+  background-color: ${(props) => props.theme.colors.black};
+  margin-top: 15px;
+  position: relative;
+  left: -15px;
+  @media ${(props) => props.theme.minWidth.md} {
+    display: none;
+  }
+`;
+
+const DesktopBottomLine = styled.div`
+  display: none;
+  @media ${(props) => props.theme.minWidth.md} {
+    display: block;
+    height: 1px;
+    width: calc(100vw);
+    margin-left: -25px;
+    background-color: ${(props) => props.theme.colors.black};
+  }
+`;
+
+const VerticalLine = styled.div`
+  width: 1px;
+  background-color: ${(props) => props.theme.colors.black};
+  height: 100%;
+  position: absolute;
+  right: -25px;
+  top: 0;
+  bottom: 0;
+`;
+
 const ProjectsList = ({ selectedCategory }) => {
   const data = useStaticQuery(
     graphql`
@@ -100,39 +130,51 @@ const ProjectsList = ({ selectedCategory }) => {
   );
 
   const projects = data.allSanityProject.nodes;
-  const filteredProjects = selectedCategory
-    ? projects.filter((project) => project.category === selectedCategory)
-    : projects;
+  const filteredProjects =
+    selectedCategory && selectedCategory !== "Tous les projets"
+      ? projects.filter((project) => project.category === selectedCategory)
+      : projects;
 
   return (
     <PageContainer>
       <StyledProjectsList>
         {filteredProjects.map((project, i) => {
           const image = getImage(project.image.asset);
-          let gridColumn;
-          let paddingLeft;
+
+          //Give different sizes to the first and last project of a 6-project series, and its tags
+          let gridColumnProject;
+          let gridColumnProjectTags;
           if (i % 6 === 0 || i % 6 === 5) {
-            gridColumn = "span 3";
+            gridColumnProject = "span 3";
+            gridColumnProjectTags = "span 5";
           } else {
-            gridColumn = "span 2";
+            gridColumnProject = "span 2";
+            gridColumnProjectTags = "span 7";
           }
 
+          let paddingLeft;
           if (i % 3 !== 0) {
-            paddingLeft = "30px";
+            paddingLeft = "25px";
           }
+
+          //Check if it is the first project in row
+          const isFirstInRow = i % 3 === 0;
 
           return (
             <StyledProjectContainer
               key={i}
-              gridColumn={gridColumn}
+              gridColumnProject={gridColumnProject}
               paddingLeft={paddingLeft}
             >
+              {isFirstInRow && <DesktopBottomLine />}
               <Grid>
                 <StyledProjectName>
                   <StyledText type="label">Projet</StyledText>
                   {project.name}
                 </StyledProjectName>
-                <StyledProjectInfo>
+                <StyledProjectInfo
+                  gridColumnProjectTags={gridColumnProjectTags}
+                >
                   <StyledText type="label">Catégorie</StyledText>
                   <StyledMobileText type="label">
                     {project.category}
@@ -147,8 +189,10 @@ const ProjectsList = ({ selectedCategory }) => {
                     {project.surface} m²
                   </StyledDesktopText>
                 </StyledProjectInfo>
+                <VerticalLine />
               </Grid>
               <StyledGatsbyImage image={image} alt={project.name} />
+              <MobileBottomLine />
             </StyledProjectContainer>
           );
         })}
