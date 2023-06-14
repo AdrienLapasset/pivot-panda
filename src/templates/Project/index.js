8533fd8102610440f8903b7a2e7b2bfc649080e2
Clément Lapasset <clement.lapasset@gmail.com>
2023-06-14 20:56:51 +0200

    Fix Project Grid Layout

--- src/templates/Project/index.js
+++ src/templates/Project/index.js
@@ -41,6 +41,10 @@ export const query = graphql`
 const StyledHeader = styled(Grid)`
   grid-template-rows: calc(50vh - 23px) 15px calc(50vh - 78px) 60px 60px 60px 1fr;
   row-gap: 15px;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-template-rows: calc(100vh - 80px) 50px 50px 50px 1fr;
+    row-gap: 25px;
+  }
 `;
 
 const HeroImage = styled(GatsbyImage)`
@@ -49,6 +53,9 @@ const HeroImage = styled(GatsbyImage)`
   height: 100%;
   width: 100%;
   border-bottom: solid ${(props) => props.theme.colors.black} 1px;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-column: span 7;
+  }
 `;
 
 const VerticalLine = styled.div`
@@ -58,26 +65,42 @@ const VerticalLine = styled.div`
   grid-row: 2 / 4;
   grid-column: 1 / 2;
   justify-self: end;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 2 / 6;
+    grid-column: 3 / 4;
+    height: calc(100% + 50px);
+  }
 `;
 
 const StyledCategory = styled(Text)`
   grid-row: 2 / 3;
   grid-column: 2 / 4;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-column: 4 / 5;
+    align-self: start;
+  }
 `;
 
 const StyledCity = styled(Text)`
   grid-row: 2 / 3;
   grid-column: 4 / 5;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-column: 5 / 6;
+    align-self: start;
+  }
 `;
 
-const StyledTitle = styled.div`
+const StyledTitle = styled(PageContainer)`
   grid-row: 3 / 4;
   grid-column: 2 / 5;
+  padding-left: 0;
   text-transform: uppercase;
   font-size: 38px;
   line-height: 45px;
   align-self: end;
   @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 4 / 5;
+    grid-column: 4 / 8;
     font-size: 70px;
     line-height: 75px;
   }
@@ -93,6 +116,13 @@ const StyledMission = styled(PageContainer)`
   grid-row: 4 / 5;
   grid-column: span 4;
   border-top: solid ${(props) => props.theme.colors.black} 1px;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 2 / 3;
+    grid-column: 1 / 3;
+    margin-left: 25px;
+    border-left: solid ${(props) => props.theme.colors.black} 1px;
+    border-right: solid ${(props) => props.theme.colors.black} 1px;
+  }
 `;
 
 const StyledContainer = styled.div`
@@ -102,12 +132,27 @@ const StyledContainer = styled.div`
   grid-template-rows: 1fr;
   grid-template-columns: repeat(4, 1fr);
   border-top: solid ${(props) => props.theme.colors.black} 1px;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 3 / 4;
+    grid-column: 1 / 3;
+    grid-template-columns: repeat(2, 1fr);
+    padding-left: 0;
+    margin-left: 25px;
+    border-left: solid ${(props) => props.theme.colors.black} 1px;
+    border-right: solid ${(props) => props.theme.colors.black} 1px;
+  }
 `;
 
 const StyledSurface = styled(PageContainer)`
   grid-row: 1 / 2;
   grid-column: 1 / 3;
   padding-right: 0;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 1 / 2;
+    grid-column: 1 / 2;
+    padding-left: 0;
+    margin-left: 25px;
+  }
 `;
 
 const SmallVerticalLine = styled.div`
@@ -115,18 +160,28 @@ const SmallVerticalLine = styled.div`
   height: calc(100% + 14px);
   background-color: ${(props) => props.theme.colors.black};
   grid-row: 1 / 2;
-  grid-column: 3 / 3;
+  grid-column: 2 / 2;
+  @media ${(props) => props.theme.minWidth.md} {
+    height: calc(100% + 10px);
+  }
 `;
 
 const StyledYear = styled(PageContainer)`
   grid-row: 1 / 2;
-  grid-column: 3 / 5;
+  grid-column: 2 / 3;
 `;
 
 const StyledAddress = styled(PageContainer)`
   grid-row: 6 / 7;
   grid-column: span 4;
   border-top: solid ${(props) => props.theme.colors.black} 1px;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 4 / 5;
+    grid-column: 1 / 3;
+    margin-left: 25px;
+    border-top: none;
+    border: solid ${(props) => props.theme.colors.black} 1px;
+  }
 `;
 
 const StyledDescription = styled(PageContainer)`
@@ -136,11 +191,18 @@ const StyledDescription = styled(PageContainer)`
   padding-top: 30px;
   padding-bottom: 30px;
   white-space: pre-line;
+  @media ${(props) => props.theme.minWidth.md} {
+    grid-row: 5 / 6;
+    grid-column: 4 / 8;
+  }
 `;
 
 const StyledText = styled(Text)`
   padding-top: 15px;
   white-space: pre-line;
+  @media ${(props) => props.theme.minWidth.md} {
+    padding-top: 0;
+  }
 `;
 
 const StyledGatsbyImage = styled(GatsbyImage)`
