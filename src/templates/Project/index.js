44f787e6c57e752ce8ed25af75e8b9117ed8f113
Clément Lapasset <clement.lapasset@gmail.com>
2023-06-14 17:45:43 +0200

    Fix Project Grid Layout

--- src/templates/Project/index.js
+++ src/templates/Project/index.js
@@ -38,86 +38,104 @@ export const query = graphql`
   }
 `;
 
-const StyledHero = styled(Grid)`
-  height: calc(100vh - 57px);
-  grid-template-rows: 1fr 30px 1fr;
-  @media ${(props) => props.theme.minWidth.md} {
-    height: 100%;
-    grid-template-rows: 1fr repeat(3, 75px) 1fr;
-  }
+const StyledHeader = styled(Grid)`
+  grid-template-rows: calc(50vh - 23px) 15px calc(50vh - 78px) 60px 60px 60px 1fr;
+  row-gap: 15px;
 `;
 
 const HeroImage = styled(GatsbyImage)`
-  grid-column: 1 / 5;
-  grid-row: span 1;
+  grid-row: 1 / 2;
+  grid-column: span 4;
   height: 100%;
   width: 100%;
   border-bottom: solid ${(props) => props.theme.colors.black} 1px;
-  @media ${(props) => props.theme.minWidth.md} {
-    height: calc(100vh - 80px);
-    grid-column: 1 / 8;
-  }
 `;
 
 const VerticalLine = styled.div`
   width: 1px;
   background-color: ${(props) => props.theme.colors.black};
-  height: 100%;
-  grid-column: 1 / 2;
+  height: calc(100% + 30px);
   grid-row: 2 / 4;
+  grid-column: 1 / 2;
   justify-self: end;
-  @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 3 / 4;
-    grid-row: 2 / 6;
-  }
 `;
 
 const StyledCategory = styled(Text)`
+  grid-row: 2 / 3;
   grid-column: 2 / 4;
-  padding-top: 15px;
-  @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 4 / 5;
-    padding-top: 0;
-  }
 `;
 
 const StyledCity = styled(Text)`
+  grid-row: 2 / 3;
   grid-column: 4 / 5;
-  padding-top: 15px;
-  @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 5 / 6;
-    padding-top: 0;
-  }
 `;
 
-const ProjectTitle = styled.div`
+const StyledTitle = styled.div`
+  grid-row: 3 / 4;
+  grid-column: 2 / 5;
   text-transform: uppercase;
   font-size: 38px;
   line-height: 45px;
-  grid-column: 2 / 5;
   align-self: end;
-  padding-bottom: 15px;
   @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 4 / 8;
-    grid-row: 4 / 5;
     font-size: 70px;
     line-height: 75px;
-    padding-bottom: 10px;
   }
 `;
 
-const HorizontalLine = styled.div`
-  height: 1px;
-  width: calc(100vw);
+// const HorizontalLine = styled.div`
+//   height: 1px;
+//   width: calc(100vw);
+//   background-color: ${(props) => props.theme.colors.black};
+// `;
+
+const StyledMission = styled(PageContainer)`
+  grid-row: 4 / 5;
+  grid-column: span 4;
+  border-top: solid ${(props) => props.theme.colors.black} 1px;
+`;
+
+const StyledContainer = styled.div`
+  grid-row: 5 / 6;
+  grid-column: span 4;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: repeat(4, 1fr);
+  border-top: solid ${(props) => props.theme.colors.black} 1px;
+`;
+
+const StyledSurface = styled(PageContainer)`
+  grid-row: 1 / 2;
+  grid-column: 1 / 3;
+  padding-right: 0;
+`;
+
+const SmallVerticalLine = styled.div`
+  width: 1px;
+  height: calc(100% + 14px);
   background-color: ${(props) => props.theme.colors.black};
+  grid-row: 1 / 2;
+  grid-column: 3 / 3;
 `;
 
-const StyledContainer = styled(PageContainer)`
-  padding-bottom: 15px;
-  @media ${(props) => props.theme.minWidth.md} {
-    grid-column: 1 / 3;
-    grid-row: 2 / 5;
-  }
+const StyledYear = styled(PageContainer)`
+  grid-row: 1 / 2;
+  grid-column: 3 / 5;
+`;
+
+const StyledAddress = styled(PageContainer)`
+  grid-row: 6 / 7;
+  grid-column: span 4;
+  border-top: solid ${(props) => props.theme.colors.black} 1px;
+`;
+
+const StyledDescription = styled(PageContainer)`
+  grid-row: 7 / 8;
+  grid-column: span 4;
+  border-top: solid ${(props) => props.theme.colors.black} 1px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  white-space: pre-line;
 `;
 
 const StyledText = styled(Text)`
@@ -125,26 +143,13 @@ const StyledText = styled(Text)`
   white-space: pre-line;
 `;
 
-const StyledDescription = styled(Text)`
-  padding-top: 30px;
-  padding-bottom: 30px;
-  white-space: pre-line;
-`;
-
-const SmallVerticalLine = styled.div`
-  width: 1px;
-  height: 100%;
-  background-color: ${(props) => props.theme.colors.black};
-  justify-self: end;
-`;
-
 const StyledGatsbyImage = styled(GatsbyImage)`
   margin: 15px 0;
 `;
 
-const ParagraphSeparator = styled(HorizontalLine)`
-  margin-top: 60px;
-`;
+// const ParagraphSeparator = styled(HorizontalLine)`
+//   margin-top: 60px;
+// `;
 
 const Project = ({ data }) => {
   const project = data.sanityProject;
@@ -153,76 +158,75 @@ const Project = ({ data }) => {
 
   return (
     <Layout>
-      <StyledHero>
+      <StyledHeader>
         <HeroImage image={image} alt={project.name} />
         <VerticalLine />
         <StyledCategory type="label">{project.category}</StyledCategory>
         <StyledCity type="label">{project.city}</StyledCity>
-        <ProjectTitle>{project.name}</ProjectTitle>
-      </StyledHero>
-      <HorizontalLine />
-      <StyledContainer>
-        <StyledText type="label">Mission Pivot Panda</StyledText>
-        <p>{project.mission}</p>
-      </StyledContainer>
-      <HorizontalLine />
-      <Grid>
+        <StyledTitle>{project.name}</StyledTitle>
+        {/* <HorizontalLine /> */}
+        <StyledMission>
+          <StyledText type="label">Mission Pivot Panda</StyledText>
+          <p>{project.mission}</p>
+        </StyledMission>
         <StyledContainer>
-          <StyledText type="label">Surface</StyledText>
-          <p>{project.surface} m²</p>
+          <StyledSurface>
+            <StyledText type="label">Surface</StyledText>
+            <p>{project.surface} m²</p>
+          </StyledSurface>
+          <SmallVerticalLine />
+          <StyledYear>
+            <StyledText type="label">Année</StyledText>
+            <p>{projectYear}</p>
+          </StyledYear>
         </StyledContainer>
-        <SmallVerticalLine />
-        <StyledContainer>
-          <StyledText type="label">Année</StyledText>
-          <p>{projectYear}</p>
-        </StyledContainer>
-      </Grid>
-      <HorizontalLine />
-      <StyledContainer>
-        <StyledText type="label">Adresse</StyledText>
-        <p>{project.address}</p>
-      </StyledContainer>
-      <HorizontalLine />
-      <StyledContainer>
-        <StyledDescription>{project.description}</StyledDescription>
-      </StyledContainer>
-      <HorizontalLine />
+        {/* <HorizontalLine /> */}
+        <StyledAddress>
+          <StyledText type="label">Adresse</StyledText>
+          <p>{project.address}</p>
+        </StyledAddress>
+        {/* <HorizontalLine /> */}
+        <StyledDescription>
+          <StyledText>{project.description}</StyledText>
+        </StyledDescription>
+        {/* <HorizontalLine /> */}
+      </StyledHeader>
       {project.contentBlocks.map((block, i) => {
         if (block.image) {
           const blockImage = getImage(block.image.asset);
           if (block.text) {
             return (
               <div key={i}>
-                <StyledContainer>
+                <PageContainer>
                   <StyledGatsbyImage
                     image={blockImage}
                     alt={`Block image ${i}`}
                   />
                   <StyledText>{block.text}</StyledText>
-                </StyledContainer>
-                <ParagraphSeparator />
+                </PageContainer>
+                {/* <ParagraphSeparator /> */}
               </div>
             );
           } else {
             return (
               <div key={i}>
-                <StyledContainer>
+                <PageContainer>
                   <StyledGatsbyImage
                     image={blockImage}
                     alt={`Block image ${i}`}
                   />
-                </StyledContainer>
-                <ParagraphSeparator />
+                </PageContainer>
+                {/* <ParagraphSeparator /> */}
               </div>
             );
           }
         } else {
           return (
             <div key={i}>
-              <StyledContainer>
+              <PageContainer>
                 <StyledText>{block.text}</StyledText>
-              </StyledContainer>
-              <ParagraphSeparator />
+              </PageContainer>
+              {/* <ParagraphSeparator /> */}
             </div>
           );
         }
