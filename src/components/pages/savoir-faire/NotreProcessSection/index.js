eb32dda0e1536d2ebfb25701694a40048f6abf42
AdrienLapasset <aflapasset@gmail.com>
2023-07-03 14:46:02 +0200

    add steps decriptions

--- src/components/pages/savoir-faire/NotreProcessSection/index.js
+++ src/components/pages/savoir-faire/NotreProcessSection/index.js
@@ -10,44 +10,80 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const StyledContainer = styled.section`
   border-bottom: ${({ theme }) => theme.border};
+  margin: 100px 0;
 `;
 const StyledGrid = styled(Grid)`
-  margin: 100px 0;
   & > div {
     &:first-of-type {
       grid-column: 1 / span 2;
       align-self: flex-start;
+      position: sticky;
+      top: 100px;
     }
     &:last-of-type {
-      grid-column: 4 / span 3;
+      grid-column: 4 / span 2;
       display: grid;
     }
   }
 `;
+const StyledStepDescriptionContainer = styled.div`
+  margin: 100px 0 250px;
+  & > div {
+    opacity: 0.5;
+    &:not(:last-child) {
+      margin-bottom: 25px;
+      padding-bottom: 125px;
+      border-bottom: ${({ theme }) => theme.border};
+    }
+    h3 {
+      font-size: 18px;
+    }
+    &:nth-child(1) {
+      opacity: ${({ currentStep }) => currentStep === 1 && "1"};
+    }
+    &:nth-child(2) {
+      opacity: ${({ currentStep }) => currentStep === 2 && "1"};
+    }
+    &:nth-child(3) {
+      opacity: ${({ currentStep }) => currentStep === 3 && "1"};
+    }
+    &:nth-child(4) {
+      opacity: ${({ currentStep }) => currentStep === 4 && "1"};
+    }
+  }
+`;
 const NotreProcessSection = () => {
-  // const step2Ref = useRef(null);
-  // const headersHeight = 400;
-  // const [currentStep, setCurrentStep] = useState(1);
+  const step1Ref = useRef(null);
+  const step2Ref = useRef(null);
+  const step3Ref = useRef(null);
+  const step4Ref = useRef(null);
+  const headersHeight = 250;
+  const [currentStep, setCurrentStep] = useState(1);
 
-  // useEffect(() => {
-  //   const handleStep = () => {
-  //     const step2Top = step2Ref.current.getBoundingClientRect().top;
-  //     const step2Bottom = step2Ref.current.getBoundingClientRect().bottom;
-  //     if (step2Top >= headersHeight) {
-  //       setCurrentStep(1);
-  //     }
-  //     if (step2Top < headersHeight) {
-  //       setCurrentStep(2);
-  //     }
-  //     if (step2Bottom <= headersHeight) {
-  //       setCurrentStep(3);
-  //     }
-  //   };
-  //   window.addEventListener("scroll", handleStep);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleStep);
-  //   };
-  // }, [step2Ref]);
+  useEffect(() => {
+    const handleStep = () => {
+      const step1Top = step1Ref.current.getBoundingClientRect().top;
+      const step2Top = step2Ref.current.getBoundingClientRect().top;
+      const step3Top = step3Ref.current.getBoundingClientRect().top;
+      const step4Top = step4Ref.current.getBoundingClientRect().top;
+      if (step1Top > headersHeight) {
+        setCurrentStep(1);
+      }
+      if (step1Top <= headersHeight) {
+        setCurrentStep(2);
+      }
+      if (step2Top <= headersHeight) {
+        setCurrentStep(3);
+      }
+      if (step3Top <= headersHeight) {
+        setCurrentStep(4);
+      }
+    };
+    window.addEventListener("scroll", handleStep);
+    return () => {
+      window.removeEventListener("scroll", handleStep);
+    };
+  }, [step1Ref, step2Ref, step3Ref, step4Ref]);
 
   return (
     <StyledContainer>
@@ -57,7 +93,48 @@ const NotreProcessSection = () => {
             <Title as="h2">Notre process</Title>
             <ProcessWheel />
           </div>
-          <div>content</div>
+          <StyledStepDescriptionContainer currentStep={currentStep}>
+            <div ref={step1Ref}>
+              <Text as="h3" type="projectTitle">
+                1. Montage et accompagnement
+              </Text>
+              <Text>
+                Projections de plusieurs scénarios adaptables au bien Chiffrage
+                détaillé et simulation budgétaire Étude de faisabilité technique
+                et urbanistique
+              </Text>
+            </div>
+            <div ref={step2Ref}>
+              <Text as="h3" type="projectTitle">
+                2. Recherche de biens uniques
+              </Text>
+              <Text>
+                Pré-visite par nos soins Selon vos critères et avec notre réseau
+                et savoir faire. Hotels particuliers Immeubles historiques Biens
+                offmarkets Plateaux haussmanniens Ensembles immobiliers
+              </Text>
+            </div>
+            <div ref={step3Ref}>
+              <Text as="h3" type="projectTitle">
+                3. Etude architecturale et technique
+              </Text>
+              <Text>
+                Pré-visite par nos soins Selon vos critères et avec notre réseau
+                et savoir faire. Hotels particuliers Immeubles historiques Biens
+                offmarkets Plateaux haussmanniens Ensembles immobiliers
+              </Text>
+            </div>
+            <div ref={step4Ref}>
+              <Text as="h3" type="projectTitle">
+                4. Montage et accompagnement
+              </Text>
+              <Text>
+                Projections de plusieurs scénarios adaptables au bien Chiffrage
+                détaillé et simulation budgétaire Étude de faisabilité technique
+                et urbanistique
+              </Text>
+            </div>
+          </StyledStepDescriptionContainer>
         </StyledGrid>
       </PageContainer>
     </StyledContainer>
