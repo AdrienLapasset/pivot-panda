e20a3829b4d903053ec66f95e3710324312c03e8
AdrienLapasset <aflapasset@gmail.com>
2023-07-04 15:34:12 +0200

    add navigation border handle

--- src/components/Layout/Navigation/index.js
+++ src/components/Layout/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import PPlogo from "assets/logos/logo-pivot-panda-paysage.svg";
 import Grid from "components/global/Grid";
@@ -13,25 +13,32 @@ const StyledNav = styled.nav`
   z-index: 2;
   padding: 15px 0;
   align-items: center;
+  border-bottom: ${({ theme }) => theme.border};
+  border-color: ${({ theme, isBorder }) =>
+    isBorder ? theme.colors.black : "white"};
+  transition: border-color 0.3s;
   @media ${(props) => props.theme.minWidth.md} {
-    padding: 25px 0;
+    padding: 20px 0;
   }
   img {
-    text-align: left;
-
     height: 26px;
     @media ${(props) => props.theme.minWidth.md} {
       height: 30px;
+      position: relative;
+      top: 2px;
     }
   }
-  a:not(.logo) {
-    font-size: 10px;
-    font-weight: 300;
-    text-transform: uppercase;
-    text-align: left;
-    @media ${(props) => props.theme.minWidth.md} {
-      text-align: right;
-      font-size: 12px;
+  a {
+    align-self: center;
+    &:not(.logo) {
+      font-size: 10px;
+      font-weight: 300;
+      text-transform: uppercase;
+      text-align: left;
+      @media ${(props) => props.theme.minWidth.md} {
+        text-align: right;
+        font-size: 12px;
+      }
     }
   }
 `;
@@ -39,8 +46,27 @@ const StyledNav = styled.nav`
 const Navigation = () => {
   const { pathname } = useLocation();
 
+  const isInitBorder =
+    pathname === "/realisations/" || pathname === "/savoir-faire/";
+
+  const [isBorder, setIsBorder] = useState(isInitBorder);
+
+  useEffect(() => {
+    const handleIsBorder = () => {
+      const scrollY = window.scrollY;
+      if (scrollY === 0 && !isInitBorder) {
+        setIsBorder(false);
+      } else {
+        setIsBorder(true);
+      }
+    };
+    window.addEventListener("scroll", handleIsBorder);
+    return () => {
+      window.removeEventListener("scroll", handleIsBorder);
+    };
+  }, []);
   return (
-    <StyledNav>
+    <StyledNav isBorder={isBorder}>
       <PageContainer>
         <Grid>
           <Link className="logo" to="/">
